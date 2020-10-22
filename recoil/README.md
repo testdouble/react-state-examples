598e443257ba92d3ffdcf5c9a390c713c94cea4e
Thomas Groshong <thomas.groshong@zendesk.com>
2020-10-22 15:01:55 -0400

    Add first recoil Example

---
+++ recoil/README.md
@@ -0,0 +1,5 @@
+# Recoil Examples
+
+Included Examples:
+
+- Simple Character counter: [simplecounter/](https://github.com/testdouble/react-state-examples/tree/main/recoil/simplecounter)
---
+++ recoil/simplecounter/src/App.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from "recoil";
+import logo from "./logo.svg";
+import "./App.css";
+
+const textState = atom({
+  key: "textState", // unique ID (with respect to other atoms/selectors)
+  default: "", // default value (aka initial value)
+});
+
+const charCountState = selector({
+  key: "charCountState", // unique ID (with respect to other atoms/selectors)
+  get: ({ get }) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+
+export default function App() {
+  return (
+    <RecoilRoot>
+      <CharacterCounter />
+    </RecoilRoot>
+  );
+}
+
+function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+}
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <>Character Count: {count}</>;
+}
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
